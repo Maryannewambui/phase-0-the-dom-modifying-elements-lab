b77ec05598f83c37136f6d637330e5669b522823
Maryannewambui <trimbleleewambui@gmail.com>
2023-09-16 21:11:48 +0300

    assignment copmlete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Maryanne is the champion';
+
+const body = document.querySelector('body');
+
+body.appendChild(newHeader);
